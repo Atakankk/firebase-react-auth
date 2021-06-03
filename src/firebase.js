dd3d129a158c0dafd47eeaa068b4c3493033e259
Atakankk <atakankrcn@gmail.com>
2021-06-03 12:24:58 +0300

    second commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
 import firebase from "firebase/app"
 import "firebase/auth"
 
+
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyAZDX0X2IK3Z4arwM6GPK7yjyOrkI8AiKM",
-    authDomain: "auth-development-1694d.firebaseapp.com",
-    databaseURL: "https://auth-development-1694d-default-rtdb.europe-west1.firebasedatabase.app/",
-    projectId: "auth-development-1694d",
-    storageBucket: "auth-development-1694d.appspot.com",
-    messagingSenderId: "374544905746",
-    appId: "1:374544905746:web:1669c91f06548004034acb"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
     
 })
 
