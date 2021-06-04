327e0f813ec37630a255626c25b2bb5ff9d41b25
Atakankk <atakankrcn@gmail.com>
2021-06-04 08:58:33 +0300

    added env

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app"
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyCLEhs_HBOtoCuKa--apa5mQX1B0sJBFQg",
-  authDomain: "auth-build-7abd6.firebaseapp.com",
-  projectId: "auth-build-7abd6",
-  storageBucket: "auth-build-7abd6.appspot.com",
-  messagingSenderId: "511939873792",
-  appId: "1:511939873792:web:22e90c19e1b72fc7501ce5"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 
 })
 
